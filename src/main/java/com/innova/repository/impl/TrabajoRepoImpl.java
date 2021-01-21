fa768fe990f0e29bec1be88a4b64fb1ad43bb553
jonathan <jonathan@innova.com>
2021-01-21 15:15:33 -0500

    modified tabajoImpl

--- src/main/java/com/innova/repository/impl/TrabajoRepoImpl.java
+++ src/main/java/com/innova/repository/impl/TrabajoRepoImpl.java
@@ -24,7 +24,7 @@ public class TrabajoRepoImpl  extends AbstractRepoImpl<Trabajo> implements Traba
     }
     @Override
     public Optional<Trabajo> findByPhone(String phone) {
-
+        
         String sql="select id, direccion, rubro,telefono from trabajo where telefono=?";
 
         return Optional.of(jdbcTemplate.queryForObject(sql,rowMapper,phone));
