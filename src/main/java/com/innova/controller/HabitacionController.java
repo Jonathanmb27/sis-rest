6e98f77f0f70ca6c6034ba19591c691f55e84286
jonathan <jonathan@innova.com>
2021-01-22 15:35:55 -0500

    add controller habitacion

--- src/main/java/com/innova/controller/HabitacionController.java
+++ src/main/java/com/innova/controller/HabitacionController.java
@@ -10,6 +10,9 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/habitaciones")
 public class HabitacionController {
@@ -21,14 +24,35 @@ public class HabitacionController {
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
     public void create(@Validated @RequestBody Habitacion habitacion, BindingResult result){
-        try{
-            if(!result.hasErrors())
-            habitacionRepo.save(habitacion);
+
+            if(!result.hasErrors()) {
+                try{
+                    habitacionRepo.save(habitacion);
+                }catch ( Exception e){
+                    throw new TrabajoException(HttpStatus.INTERNAL_SERVER_ERROR,"Unexpected Error create");
+                }
+            }
             else
-                System.out.println("erro validation!!!!");
+                throw new TrabajoException(HttpStatus.BAD_REQUEST,"Field Validation error to create");
+    }
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public List<Habitacion>findAll(){
+        try{
+            return habitacionRepo.findAll();
         }catch (Exception e){
-            throw new TrabajoException(HttpStatus.BAD_REQUEST,"Unexpected Error");
-            //System.out.println("errorroororo!!");
+            throw new TrabajoException(HttpStatus.INTERNAL_SERVER_ERROR,"Unexpected Error list all");
+        }
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable long id){
+        Optional<Habitacion> habitacionFind=habitacionRepo.findById(id);
+        if(habitacionFind.isPresent()){
+            habitacionRepo.deleteById(id);
+        }else{
+            throw new TrabajoException(HttpStatus.BAD_REQUEST,"Don not exist habitacion with id: "+id);
         }
 
 
--- src/main/java/com/innova/repository/HabitacionRepo.java
+++ src/main/java/com/innova/repository/HabitacionRepo.java
@@ -2,7 +2,12 @@ package com.innova.repository;
 
 import com.innova.domain.Habitacion;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface HabitacionRepo extends AbstractRepo<Habitacion>{
 
 
+    List<Habitacion> findAll();
+    Optional<Habitacion> update();
 }
